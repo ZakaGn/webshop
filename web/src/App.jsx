79d3b649806542ab72c97b5d9fc001431a5a4658
Zaka_Gn <1@zaka.se>
2024-04-01 00:31:43 -0400

    deleting logs

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -21,18 +21,6 @@ function App(){
 		return () => {isMounted = false}
 	}, [])
 
-	useEffect(() => {
-		console.log('App._____________useEffect: user:', user)
-	}, [user])
-
-	useEffect(() => {
-		console.log('App._____________useEffect: cart:', cart)
-	}, [cart])
-
-	useEffect(() => {
-		console.log('App._____________useEffect: orders:', orders)
-	}, [orders])
-
 	const fetchData = async() => {
 		await getUser()
 		await getCart()
@@ -40,44 +28,32 @@ function App(){
 	}
 
 	const getUser = async() => {
-		console.log('App.js: getUser() started')
 		if(!UserService.isAuthenticated()) return
-		console.log('App.js: getUser()')
 		try{
 			const userData = await UserService.getUser()
-			console.log('App.js: getUser() userData:', userData)
 			setUser(new User(userData))
 		}catch(error){
-			console.log('App.js: getUser() error:', error)
 			toast.error(error.data?.message || 'Failed to fetch user')
 			UserService.logout()
 		}
 	}
 
 	const getCart = async() => {
-		console.log('App.js: getCart() started')
 		if(!UserService.isAuthenticated()) return
-		console.log('App.js: getCart()')
 		try{
 			const cartData = await CartService.getCart()
-			console.log('App.js: getCart() cartData:', cartData)
 			setCart(new Cart(cartData))
 		}catch(error){
-			console.log('App.js: getCart() error:', error)
 			toast.error(error.response?.data?.message || 'Failed to fetch cart')
 		}
 	}
 
 	const getUserOrders = async() => {
-		console.log('App.js: ====================> getUserOrders() started')
 		if(!UserService.isAuthenticated()) return
-		console.log('App.js: getUserOrders()')
 		try{
 			const orderData = await OrderService.getUserOrders()
-			console.log('App.js: getUserOrders() orderData:', orderData)
 			setOrders(orderData.map(order => new Order(order)))
 		}catch(error){
-			console.log('App.js: getUserOrders() error:', error)
 			toast.error(error.response?.data?.message || 'Failed to fetch orders')
 		}
 	}
